77722b4d55582ee856eff6c18c6bbff503d6e2fd
anna_kosolapova1 <anna-denils@yandex.ru>
2022-12-28 09:23:11 +0100

    homework 2

---
+++ src/test/java/hw2/TestEx1.java
@@ -0,0 +1,127 @@
+package hw2;
+
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEx1 extends ChromeTest{
+
+    @Test
+    public void TestsEx1(){
+        homepage();
+        login();
+        header();
+        icons();
+        iconsText();
+        frameButton();
+        leftSection();
+    }
+
+    //step 2 - Assert Browser title
+    public void homepage(){
+        assertEquals(driver.getTitle(), "Home Page");
+    }
+
+    //step 3 - Perform login
+    //step 4 - Assert Username is loggined
+    public void login(){
+        driver.findElement(By.id("user-icon")).click();
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+        driver.findElement(By.id("login-button")).click();
+        WebElement loginname = driver.findElement(By.cssSelector("#user-name"));
+        assertEquals(loginname.getText(), "ROMAN IOVLEV");
+    }
+
+    //step 5 - Assert that there are 4 items on the header section are displayed, and they have proper texts
+
+    @Rule
+    public JUnitSoftAssertions softAssertions = new JUnitSoftAssertions();
+
+    public void header(){
+        WebElement headerHome = driver.findElement(By.linkText("Home"));
+        WebElement headerForm = driver.findElement(By.linkText("Contact form"));
+        WebElement headerService = driver.findElement(By.linkText("Service"));
+        WebElement headerMC = driver.findElement(By.linkText("Metals & Colors"));
+        List<WebElement> headers = driver.findElements(By.cssSelector("ul[class='uui-navigation nav navbar-nav m-l8']>li"));
+        int headerCount = headers.size();
+        assertThat(headerCount).isEqualTo(4);
+        softAssertions.assertThat(headerHome.isDisplayed());
+        softAssertions.assertThat(headerForm.isDisplayed());
+        softAssertions.assertThat(headerService.isDisplayed());
+        softAssertions.assertThat(headerMC.isDisplayed());
+    }
+
+    //step 6 - Assert that there are 4 images on the Index Page, and they are displayed
+    public void icons(){
+        WebElement iconPractice = driver.findElement(By.cssSelector(".benefits > div:nth-child(1) > div > div > span"));
+        WebElement iconCustom = driver.findElement(By.cssSelector(".benefits > div:nth-child(2) > div > div > span"));
+        WebElement iconMulti = driver.findElement(By.cssSelector(".benefits > div:nth-child(3) > div > div > span"));
+        WebElement iconBase = driver.findElement(By.cssSelector(".benefits > div:nth-child(4) > div > div > span"));
+        List<WebElement> icons = driver.findElements(By.className("benefit-icon"));
+        int iconCount = icons.size();
+        assertThat(iconCount).isEqualTo(4);
+        iconPractice.isDisplayed();
+        iconCustom.isDisplayed();
+        softAssertions.assertThat(iconMulti.isDisplayed());
+        softAssertions.assertThat(iconBase.isDisplayed());
+    }
+
+    //step 7 - Assert that there are 4 texts on the Index Page under icons and they have proper text
+    public void iconsText(){
+        List<String> expText = new ArrayList<>();
+        expText.add("To include good practices\n" + "and ideas from successful\n" + "EPAM project");
+        expText.add("To be flexible and\n" + "customizable ");
+        expText.add("To be multiplatform ");
+        expText.add("Already have good base\n" + "(about 20 internal and\n" + "some external projects),\n"
+                + "wish to get more…");
+        List<WebElement> curText = driver.findElements(By.className("benefit-icon"));
+        softAssertions.assertThat(expText).isEqualTo(curText);
+        int textCount = curText.size();
+        softAssertions.assertThat(textCount).isEqualTo(4);
+    }
+
+    //step 8 - Assert that there is the iframe with “Frame Button” exist
+    //step 9 - Switch to the iframe and check that there is “Frame Button” in the iframe
+    //step 10 - Switch to original window back
+    public void frameButton(){
+        driver.switchTo().frame("frame");
+        WebElement frameButton = driver.findElement(By.id("frame-button"));
+        frameButton.isDisplayed();
+        driver.switchTo().defaultContent();
+    }
+
+    //step 11 - Assert that there are 5 items in the Left Section are displayed, and they have proper text
+    public void leftSection() {
+        WebElement leftHome = driver.findElement(By.cssSelector("ul > li:nth-child(1)"));
+        WebElement leftContact = driver.findElement
+                (By.xpath("//*/ul[@class=\"sidebar-menu left\"]/li[2]"));
+        WebElement leftService = driver.findElement(By.cssSelector("ul > li:nth-child(3)"));
+        WebElement leftMC = driver.findElement(By.xpath("//*/ul[@class=\"sidebar-menu left\"]/li[4]"));
+        WebElement leftElement = driver.findElement
+                (By.cssSelector("ul > li:nth-child(5)"));
+        List<WebElement> leftSection = driver.findElements(By.cssSelector("ul[class='sidebar-menu left']>li"));
+        int leftSectionCount = leftSection.size();
+        softAssertions.assertThat(leftSectionCount).isEqualTo(5);
+        leftHome.isDisplayed();
+        leftContact.isDisplayed();
+        leftService.isDisplayed();
+        leftMC.isDisplayed();
+        leftElement.isDisplayed();
+        softAssertions.assertThat(leftHome).isEqualTo("Home");
+        softAssertions.assertThat(leftContact).isEqualTo("Contact form");
+        softAssertions.assertThat(leftService).isEqualTo("Service");
+        softAssertions.assertThat(leftMC).isEqualTo("Metals & Colors");
+        softAssertions.assertThat(leftElement).isEqualTo("Elements packs");
+    }
+
+}
+
---
+++ src/test/java/hw2/TestEx2.java
@@ -0,0 +1,94 @@
+package hw2;
+
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEx2 extends ChromeTest{
+    @Rule
+    public JUnitSoftAssertions softAssertions = new JUnitSoftAssertions();
+
+    @Test
+    public void TestsEx2(){
+        homepage();
+        login();
+        difElements();
+        checkboxWaterWind();
+        buttonSelen();
+        dropdownYellow();
+        log();
+    }
+
+    //step 2 - Assert Browser title
+    public void homepage(){
+        assertEquals(driver.getTitle(), "Home Page");
+    }
+
+    //step 3 - Perform login
+    //step 4 - Assert Username is loggined
+    public void login(){
+        driver.findElement(By.id("user-icon")).click();
+        driver.findElement(By.id("name")).sendKeys("Roman");
+        driver.findElement(By.id("password")).sendKeys("Jdi1234");
+        driver.findElement(By.id("login-button")).click();
+        WebElement loginname = driver.findElement(By.cssSelector("#user-name"));
+        assertEquals(loginname.getText(), "ROMAN IOVLEV");
+    }
+
+    //step 5 - Open through the header menu Service -> Different Elements Page
+    public void difElements(){
+        driver.findElement(By.className("dropdown-toggle")).click();
+        //driver.findElement(By.linkText("Service")).click();
+        driver.findElement(By.partialLinkText("DIFFERENT ELEMENTS")).click();
+        //cssSelector("ul#dropdown-menu li:nth-of-type(8)")
+        assertEquals(driver.getTitle(), "Different Elements");
+    }
+
+    //step 6 - Select checkboxes Water, Wind
+    public void checkboxWaterWind(){
+        WebElement water = driver.findElement(By.xpath("//label[text()[contains(., ' Water')]]"));
+        water.click();
+        water.isSelected();
+        WebElement wind = driver.findElement(By.xpath("//label[text()[contains(., ' Wind')]]"));
+        wind.click();
+        wind.isSelected();
+    }
+
+    //step 7 - Select radio Selen
+    public void buttonSelen(){
+        WebElement selen = driver.findElement(By.xpath("//label[text()[contains(., ' Selen')]]"));
+        selen.click();
+        selen.isSelected();
+    }
+
+    //step 8 - Select in dropdown Yellow
+    public void dropdownYellow(){
+        WebElement yellow = driver.findElement(By.cssSelector("select > option:nth-child(4)"));
+        yellow.click();
+        softAssertions.assertThat(yellow.isSelected());
+
+    }
+
+    //step 9 -Assert that
+    //• for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+    //• for radio button there is a log row and value is corresponded to the status of radio button
+    //• for dropdown there is a log row and value is corresponded to the selected value.
+    public void log(){
+        WebElement logWater = driver.findElement
+                (By.xpath("//*[contains(text(),'Water: condition changed to true')]"));
+        logWater.isDisplayed();
+        WebElement logWind = driver.findElement
+                (By.xpath("//*[contains(text(),'Wind: condition changed to true')]"));
+        logWind.isDisplayed();
+        WebElement logSelen = driver.findElement
+                (By.xpath("//*[contains(text(),'metal: value changed to  Selen')]"));
+        logSelen.isDisplayed();
+        WebElement logYellow = driver.findElement
+                (By.xpath("//*[contains(text(),'Colors: value changed to Yellow')]"));
+        logYellow.isDisplayed();
+    }
+}
