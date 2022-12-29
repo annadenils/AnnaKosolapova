dc1f37c81bed0fc8ebe300647cbdf5fe99091e9d
anna_kosolapova1 <anna-denils@yandex.ru>
2022-12-29 17:35:38 +0100

    homework 2 (change start driver)

--- src/test/java/hw2/ChromeTest.java
+++ src/test/java/hw2/ChromeTest.java
@@ -3,25 +3,21 @@ package hw2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 class ChromeTest {
     public static final String URL = "https://jdi-testing.github.io/jdi-light/index.html";
     WebDriver driver;
-
-    @BeforeClass
-    public void beforeClass(){
-        driver = new ChromeDriver();
-    }
-
     @BeforeMethod
     public void setUp(){
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL);
     }
 
-    @AfterClass
+    @AfterMethod
     void teardown() {
         driver.quit();
     }
